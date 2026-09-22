b0d3284135820b067f3ea01e22718a185b944a32
aryannsainiii-hub <aryannsainiii@gmail.com>
2026-09-22 13:46:11 +0530

    QUICK-SORT

--- SORT/quicksort.cpp
+++ SORT/quicksort.cpp
@@ -1,44 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void swap(int a,int b){
-    int temp;
-    temp = a;
-    a=b;
-    b=temp;
-}
-int partation(int arr[],int low,int high){
-    int pivot =  arr[low];
-    int i = low+1;
-    int j = high-1;
-    while(true){
-        while(i<=pivot){
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
+            swap(arr[i], arr[j]);
         }
-        while(j>=pivot){
-            j--;
-        }
-        if(i<j){
-            swap(arr[i],arr[j]);
-        }else{
-            swap(arr[pivot],arr[j]);
-        }
-        return j;
     }
-};
-void quicksort(int arr[],int low,int high){
-    quicksort(arr,0,low-1);
-    quicksort(arr,low+1,high);
+
+    swap(arr[i + 1], arr[high]);
+
+    return i + 1;
 }
 
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
 
-int main(){
-    int arr[]={10,7,8,9,1,5};
-    int n =6;
-
-    quicksort(arr,0,n-1);
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = 6;
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
